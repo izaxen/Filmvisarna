b2b9a200a1352ca8b335bbc88610fda3d9c9bf2c
Martin Isaksen <izaxen@telia.com>
2021-03-11 11:13:23 +0100

    Show seat is ok. Styling left

--- saloons.js
+++ saloons.js
@@ -23,20 +23,21 @@ function showSeats() {
   let tempRow = tokyo.seatsPerRow
   let row
   let seat
+  let seatCounter = 0
 
   for (let i = 0; i < tempRow.length; i++) {
     for (let j = 0; j < tempRow[i]; j++) {
       // add each of the seats to seat
+      seatCounter++
       if (j === 0) {
-        seat = `<div class="seat" id="seat-${j + 1}"><h2>Row${i + 1} seat ${j + 1}</h2></div>`
+        seat = `<div class="seat" id="seat-${seatCounter}">${seatCounter}</div>`
       }
-      else { seat += `<div class="seat" id="seat-${j + 1}"><h2>Row${i + 1} seat ${j + 1}</h2></div>` }
+      else { seat += `<div class="seat" id="seat-${seatCounter}">${seatCounter}</div>` }
     }
     // Create a div for every row
     row = `<div class="row" id="row-${i + 1}">${seat}</div>`
     console.log(row)
     $('.main-box').append(row)
-    seat -= seat
   }
 
 }
