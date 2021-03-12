1fac84299f6108d486ffd973d4e74e9e1089f92d
Martin Isaksen <izaxen@telia.com>
2021-03-12 20:19:34 +0100

    Added new functions for rendering the seats and cleaned up the code

--- js/saloons/saloons.js
+++ js/saloons/saloons.js
@@ -1,64 +1,77 @@
 export default class SaloonPage {
-  //getSaloons('tokyo')
-
-  openSeats(place) {
+  
+  openSeats(place) {        //Hardcoded array for testing the disabling of the seats
     let openSeats = [
       "true",
       "false",
-        "false"
+      "false",
+        "true",
+      "false",
+      "false",
+      "true",
+      "false",
+        "true"
     ]
     return openSeats[place]
   }
 
-  async getSaloons(saloonChoice) {
+  async getSaloons(saloonChoice) {    //Loading JSOn library with saloon info and returns choosen saloon.
     let saloons = await $.getJSON("/js/saloons/saloons.json");
-
     if (saloonChoice === 'tokyo') {
-      return this.showSeats(saloons[0]);
+      return this.renderSeats(saloons[0]);
     } 
-    else return this.showSeats(saloons[1]);
+    else return this.renderSeats(saloons[1]);
   }
 
-  showSeats(saloon) {
+  renderSeats(saloon) {       //Rendering the seats in the selected saloon
     let tempRow = saloon.seatsPerRow;
     let seat;
     let seatCounter = 0;
 
-    $('main').html(`<div class="saloon-box"></div>`);
-    this.showScreen(saloon);
+    $('main').html(`<div class="saloon-box"></div>`);     //Adding main workspace
+    this.renderScreener(saloon);      //Adding a screener at the top of main workspace
 
-    for (let i = 0; i < tempRow.length; i++) {
-      for (let j = 0; j < tempRow[i]; j++) {
-        // add each of the seats to seat
+    for (let i = 0; i < tempRow.length; i++) {      //Looping through the rows
+      for (let j = 0; j < tempRow[i]; j++) {            //Looping through each seat in the row
         seatCounter++;
         
-        if (j === 0) {
-          if (this.openSeats(0) === 'true') {
-            console.log('Testing If')
-            seat = `<input type="checkbox" class="seat" id="seat-${seatCounter - 1
-              } value="${seatCounter}" disabled>
-        <label class="seat">${seatCounter}</label>`;
+        if (j === 0) {    //To find the start of a new row
+          if (this.openSeats(seatCounter-1) === 'true') {       //Control if the seat is available or taken and adding them to the first place in the row (seat=)
+            seat = this.addSeatRowDisabeld(seatCounter)
           }
           else {
-            seat = `<input type="checkbox" class="seat" id="seat-${seatCounter - 1
-              } value="${seatCounter}">
-        <label class="seat">${seatCounter}</label>`;
+            seat = this.addSeatRowActive(seatCounter)
           }
-        } else {
-          seat += `<input type="checkbox" class="seat" id="seat-${
-            seatCounter - 1
-          } value="${seatCounter}">
-        <label class="seat">${seatCounter}</label>`;
+        }
+        else {
+          if (this.openSeats(seatCounter - 1) === 'true') {       //Looping through the rest of the chairs and adding them to the row. (seat +=)
+            seat += this.addSeatRowDisabeld(seatCounter)
+          }
+          else {
+            seat += this.addSeatRowActive(seatCounter)
+          }
         }
       }
-      // Create a div for every row
-      $(".saloon-box").append(
+      
+      $(".saloon-box").append(      //Adding a row with seats to the saloonbox
         `<div class="row" id="row-${i + 1}">${seat}</div>`
       );
     }
   }
   
-  showScreen(saloon) {
+  renderScreener(saloon) {
     $(".saloon-box").prepend(`<div class="screen">Saloon ${saloon.name}</div`);
+  }
+  
+  addSeatRowDisabeld(seatCounter) {   
+    return `<input type="checkbox" class="seat" id="seat-${seatCounter - 1
+              } value="${seatCounter}" disabled>
+        <label class="seat">${seatCounter}</label>`;
+  }
+  
+  addSeatRowActive(seatCounter) {
+    return `<input type="checkbox" class="seat" id="seat-${seatCounter - 1
+              } value="${seatCounter}">
+        <label class="seat">${seatCounter}</label>`;
   }
 }
