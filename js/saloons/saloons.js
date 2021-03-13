b231510e8f87edc2cd442c550c2065299c88370f
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-03-13 12:30:17 +0100

    Stylized the booking page
    Added a seat-box
    Added backgrounds and font-styling
    Fixed issue where image would repeat on bigger screens

--- js/saloons/saloons.js
+++ js/saloons/saloons.js
@@ -1,16 +1,16 @@
 export default class SaloonPage {
-  
+
   openSeats(place) {        //Hardcoded array for testing the disabling of the seats
     let openSeats = [
       "true",
       "false",
       "false",
-        "true",
+      "true",
       "false",
       "false",
       "true",
       "false",
-        "true"
+      "true"
     ]
     return openSeats[place]
   }
@@ -19,7 +19,7 @@ export default class SaloonPage {
     let saloons = await $.getJSON("/js/saloons/saloons.json");
     if (saloonChoice === 'tokyo') {
       return this.renderSeats(saloons[0]);
-    } 
+    }
     else return this.renderSeats(saloons[1]);
   }
 
@@ -28,15 +28,15 @@ export default class SaloonPage {
     let seat;
     let seatCounter = 0;
 
-    $('main').html(`<div class="saloon-box"></div>`);     //Adding main workspace
+    $('main').html(`<div class="saloon-box"><div class="seat-box"></div></div>`);     //Adding main workspace
     this.renderScreener(saloon);      //Adding a screener at the top of main workspace
 
     for (let i = 0; i < tempRow.length; i++) {      //Looping through the rows
       for (let j = 0; j < tempRow[i]; j++) {            //Looping through each seat in the row
         seatCounter++;
-        
+
         if (j === 0) {    //To find the start of a new row
-          if (this.openSeats(seatCounter-1) === 'true') {       //Control if the seat is available or taken and adding them to the first place in the row (seat=)
+          if (this.openSeats(seatCounter - 1) === 'true') {       //Control if the seat is available or taken and adding them to the first place in the row (seat=)
             seat = this.addSeatRowDisabeld(seatCounter)
           }
           else {
@@ -52,26 +52,26 @@ export default class SaloonPage {
           }
         }
       }
-      
-      $(".saloon-box").append(      //Adding a row with seats to the saloonbox
+
+      $(".seat-box").append(      //Adding a row with seats to the saloonbox
         `<div class="row" id="row-${i + 1}">${seat}</div>`
       );
     }
   }
-  
+
   renderScreener(saloon) {
-    $(".saloon-box").prepend(`<div class="screen">Saloon ${saloon.name}</div`);
+    $(".seat-box").prepend(`<div class="saloon-title">Saloon ${saloon.name}</div`);
   }
-  
-  addSeatRowDisabeld(seatCounter) {   
+
+  addSeatRowDisabeld(seatCounter) {
     return `<input type="checkbox" class="seat" id="seat-${seatCounter - 1
-              } value="${seatCounter}" disabled>
+      } value="${seatCounter}" disabled>
         <label class="seat">${seatCounter}</label>`;
   }
-  
+
   addSeatRowActive(seatCounter) {
     return `<input type="checkbox" class="seat" id="seat-${seatCounter - 1
-              } value="${seatCounter}">
+      } value="${seatCounter}">
         <label class="seat">${seatCounter}</label>`;
   }
 }
