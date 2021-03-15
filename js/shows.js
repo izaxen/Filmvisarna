2fe5c61b002270e6661ba7538ec7ee921cd30e2b
Daniel Labbé <daniel@labbe.de>
2021-03-15 11:39:43 +0100

    minor fix

--- js/shows.js
+++ js/shows.js
@@ -45,6 +45,7 @@ export default class Shows {
   renderSelectionOfShows(start, range) {
     $('main').empty()
     $('main').append('<br><br><br><br><br>') //TODO Remove. Header workaround for the current layout
+
     $('main').append(`<img class="arrow" id="left-arrow" src="../images/left_bracket_white.png">`)
     for (let i = start; i < range; i++) {
       $('main').append(`<div id="shows"><p>
