cba96b81ab535bc38b374cafdb87bfbfb918b71a
Martin Isaksen <izaxen@telia.com>
2021-03-30 09:07:15 +0200

    Updated frontpage with todays shows, trailer and book ur candy

--- js/pages/frontpage.js
+++ js/pages/frontpage.js
@@ -17,14 +17,14 @@ export default class FrontPage {
           <h1>${this.movies[randomMovie].title}</h1>
         </div>
         <iframe id="front-movie-trailer" src="${this.movies[randomMovie].youtubeTrailers}&autoplay=1&mute=1"></iframe>
-        <div class="candy-bar">
-          <div id="candy-text"><h3>Don´t forget your candy</h3>
-          </div>
-        </div>
+        
         <div class="front-footer">
         </div>
       </div>
-          
+      <div class="candy-bar">
+        <div id="candy-text"><h3>Book your candy</h3>
+        </div>
+      </div>
       <div class="moving-candy">
         <div class="front-moving-popcorns">
           <img id="front-popcorn" src="https://images.unsplash.com/photo-1585647347483-22b66260dfff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="popcorn">
@@ -38,12 +38,6 @@ export default class FrontPage {
           <img id="front-gummibears" src="https://images.unsplash.com/photo-1547097465-617b04e11bb2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="gummibears">
         </div>
       </div>
-            
-      
-      <div id="lights">
-        <div class="bulb">*</div>
-        <div class="bulb">*</div>
-      </div>
       
     </div>
     `)
@@ -54,9 +48,11 @@ export default class FrontPage {
   }
 
   showShows() {
-  let dailyShows = []
+    let dailyShows = []
+    
+     
 
-    $(".front-page").append(/*html*/`<div class="front-shows"><h3>${this.getTodaysDate()}</h3></div>`)
+    $(".front-page").append(/*html*/`<div class="front-shows"><h3><div class="bulb">*</div>${this.getTodaysDate()}<div class="bulb">*</div></h3></div>`)
     $(".front-shows").append(/*html*/`<div class="front-daily-shows"><h2>Today's show</h2></div>`)
     for (let show of this.shows) {
       if (show.date === this.getTodaysDate())
@@ -85,3 +81,8 @@ export default class FrontPage {
   return year + '-' + month + '-' + day
 }
 }
+
+{/* <div id="lights">
+        <div class="bulb">*</div>
+        <div class="bulb">*</div>
+      </div> */}
