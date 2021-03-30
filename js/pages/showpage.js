fb622b7a9268599f45abeeaf124aefcead129126
AndreElliot <andre_elliot@hotmail.com>
2021-03-30 15:14:56 +0200

    Added a age filter
    Added "age" to each show in shows.json
    
    Added some more if else statements in the method "filterShows" in shows.js.
    
    Added a age filter in showpage.js

--- js/pages/showpage.js
+++ js/pages/showpage.js
@@ -15,9 +15,52 @@ export default class ShowPage {
     this.shows.loadJsonAndRenderShows()
 
     $('.booking-shows').append(/*html*/`</div>`)
-    $('.show-page').prepend(/*html*/`<aside class = "filter-menu"><div><label for="day-filter">Day: </label><select name="day-filter" id="day-filter" class="filter-selector"></select></div><div><label for="month-filter">Month: </label><select name="month-filter" id="month-filter" class="filter-selector"></select></div><button id="filter-button" class="submit-selector">Choose filter</button></aside>`)
+    $('.show-page').prepend(/*html*/`<aside class = "filter-menu"><div><label for="age-filter">Age: </label><select name="age-filter" id="age-filter" class="filter-selector"></select></div><div><label for="day-filter">Day: </label><select name="day-filter" id="day-filter" class="filter-selector"></select></div><div><label for="month-filter">Month: </label><select name="month-filter" id="month-filter" class="filter-selector"></select></div><button id="filter-button" class="submit-selector">Choose filter</button></aside>`)
+
+    // $('.show-page').prepend(/*html*/`<button id="ageFilter">Age</button>`)
+    // $('main').on('click', '#ageFilter', async () => {
+    //   const json = await JSON._load("../json/shows.json");
+    //   const far = json.map((obj) => {
+    //     switch (obj.film) {
+    //       case "Braveheart":
+    //         return { ...obj, age: 15 };
+    //         break;
+    //       case "Bröderna Lejonhjärta":
+    //         return { ...obj, age: 11 };
+    //         break;
+    //       case "The Secret Life of Walter Mitty":
+    //         return { ...obj, age: 11 };
+    //         break;
+    //       case "You Don't Mess with the Zohan":
+    //         return { ...obj, age: 11 };
+    //         break;
+    //       case "Godzilla vs. Kong":
+    //         return { ...obj, age: 13 };
+    //         break;
+    //       case "The Shawshank Redemption":
+    //         return { ...obj, age: 15 };
+    //         break;
+    //       default:
+    //         return obj;
+    //         break;
+    //     }
+    //   })
+    //   console.log(far)
+    //   JSON._save("../json/shows.json", far);
+    // })
+    
+    let ageFilter = /*html*/ `<option>-</option>`
     let dayFilter = /*html*/ `<option>-</option>`
     let monthFilter = /*html*/ `<option>-</option>`
+    
+    for (let i = 1; i < 19; i++){
+      if(i === 18){
+        ageFilter += /*html*/`<option>${'18+'}</option>`
+      }
+      else {
+        ageFilter += `<option>${i}</option>`
+      }
+    }
 
     for (let i = 1; i < 32; i++) {
       if (i < 10) {
@@ -36,7 +79,7 @@ export default class ShowPage {
       }
 
     }
-
+    $('#age-filter').append(ageFilter)
     $('#day-filter').append(dayFilter)
     $('#month-filter').append(monthFilter)
   }
@@ -46,14 +89,21 @@ export default class ShowPage {
   }
 
   getFilteredShows() {
+    let chosenAge = $('#age-filter').find('option:selected').text()
     let chosenDay = $('#day-filter').find('option:selected').text()
     let chosenMonth = $('#month-filter').find('option:selected').text()
-    console.log('chosenMonth', chosenMonth, 'chosenDay', chosenDay)
-    if (chosenMonth !== '-' && chosenDay !== '-') {
+    console.log('chosenMonth', chosenMonth, 'chosenDay', chosenDay, 'chosenAge', chosenAge)
+    if(chosenMonth !== '-' && chosenDay !== '-' && chosenAge !== '-'){
+      let chosenDateAge;
+      let chosenDate = '2021-' + chosenMonth + '-' + chosenDay;
+      chosenDateAge = {chosenDate, chosenAge};
+      this.shows.filterShows('age&date', chosenDateAge)
+    } else if (chosenMonth !== '-' && chosenDay !== '-') {
       let chosenDate = '2021-' + chosenMonth + '-' + chosenDay
       this.shows.filterShows(DATE_FILTER, chosenDate)
-    }
-    else {
+    } else if(chosenAge !== '-'){
+      this.shows.filterShows('age', chosenAge)
+    } else {
       console.log('else')
       this.shows.filterShows(null, null)
     }
