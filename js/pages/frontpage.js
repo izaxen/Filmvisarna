54c61919e252cbfd5ad801dc11758e5900d61ee1
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-03-31 23:36:05 +0200

    Continued styling frontPage

--- js/pages/frontpage.js
+++ js/pages/frontpage.js
@@ -12,36 +12,29 @@ export default class FrontPage {
     $("main").html(/*html */ `
     
     <div class="front-page">
-      <div class="front-movie-trailer-box">
-        <div class="front-movie-header">
-          <h1>${this.movies[randomMovie].title}</h1>
-        </div>
-        <iframe id="front-movie-trailer" src="${this.movies[randomMovie].youtubeTrailers}&autoplay=1&mute=1"></iframe>
-        
-      </div>
-      
+        <iframe frameBorder ="0" id="front-movie-trailer" src="${this.movies[randomMovie].youtubeTrailers}&autoplay=1&mute=1&controls=0&showinfo=0&autohide=1"></iframe>`
+    )
+
+    this.showShows()
+
+    $('.front-shows').append(/*html*/`
       <div class="candy-display">
         <div class="candy-bar">
           <div id="candy-text"><h3>Book your candy</h3></div>
-        
         <div class="moving-candy">
           <div class="front-moving-popcorns">
             <img id="front-popcorn" src="https://images.unsplash.com/photo-1585647347483-22b66260dfff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="popcorn">
           </div>
-          
           <div class="front-moving-cola">
             <img id="front-coca-cola" src="https://images.unsplash.com/photo-1568739319466-dff6d8fc2a41?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80" alt="coca-cola">
           </div>
-          
           <div class="front-moving-bears">
             <img id="front-gummibears" src="https://images.unsplash.com/photo-1547097465-617b04e11bb2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="gummibears">
-            </div>
           </div>
         </div>
-      </div>    
-    </div>
+        </div> 
     `)
-    this.showShows()
+
   }
   randomNumberGenerator() {
     return Math.floor(Math.random() * 6)
@@ -52,8 +45,8 @@ export default class FrontPage {
 
 
 
-    $(".front-page").append(/*html*/`<div class="front-shows"><h3><div class="bulb">*</div>${this.getTodaysDate()}<div class="bulb">*</div></h3></div>`)
-    $(".front-shows").append(/*html*/`<div class="front-daily-shows"><h2>Today's show</h2></div>`)
+    $(".front-page").append(/*html*/`<div class="front-filter"></div><div class="front-shows"><h3><div class="bulb">*</div>${this.getTodaysDate()}<div class="bulb">*</div></h3></div>`)
+    $(".front-shows").append(/*html*/`<div class="front-daily-shows"><h2>Today's shows</h2><div class="shows-today"></div></div>`)
     for (let show of this.shows) {
       if (show.date === this.getTodaysDate()) {
         dailyShows.push(show)
@@ -61,7 +54,7 @@ export default class FrontPage {
     }
     for (let i = 0; i < dailyShows.length; i++) {
 
-      $(".front-shows").append(/*html*/`<div class="front-daily-shows"><p>${dailyShows[i].film}</p><p>${dailyShows[i].auditorium}</p><p>${dailyShows[i].time}:00</p></div>`)
+      $(".shows-today").append(/*html*/`<div class="showinfo"><p>${dailyShows[i].film}</p><p>${dailyShows[i].auditorium}</p><p>${dailyShows[i].time}:00</p></div>`)
     }
 
   }
