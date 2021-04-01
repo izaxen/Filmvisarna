f1b87ad9f3e2891c2d46bee6983b30f4c92462d3
Martin Isaksen <izaxen@telia.com>
2021-04-01 20:52:40 +0200

    Created a new class to lift out some bigger parts

---
+++ js/pages/seatSelection.js
@@ -0,0 +1,34 @@
+
+export default class SeatSelection {
+
+    getBestSeat(currentShow, selectedTypes) {
+    this.currentShow = currentShow
+    this.selectedTypes = selectedTypes
+    
+    if (this.currentShow.takenSeats.length > 50) {
+      
+
+      
+      return
+    }
+
+
+
+
+
+
+
+
+
+
+  
+  }
+
+  bestSeatsBigSaloon() {
+    
+  }
+
+  bestSeatsSmallSaloon() {
+    
+  }
+}
