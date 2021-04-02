9d38446c9ae3a24d118e01d16bb72513b0a7b43e
Martin Isaksen <izaxen@telia.com>
2021-04-02 15:41:55 +0200

    Fully working Bigsaloon with best seats

--- js/pages/seatSelection.js
+++ js/pages/seatSelection.js
@@ -6,31 +6,58 @@ export default class SeatSelection {
     this.currentShow = currentShow
     this.selectedTypes = selectedTypes
     let bestSeatsBigSaloon = this.bestSeatsBigSaloon()
+    let centerSeats = 0
     
-    
-    
-    
-    //Glöm inte att skapa IF för att kontrollera att säten inte går utanför 
-    
-
     if (this.currentShow.takenSeats.length > 50) {
-      for (let i = 0; i < bestSeatsBigSaloon.length; i++){
+      for (let i = 0; i < bestSeatsBigSaloon.length; i++) {
         let seats = bestSeatsBigSaloon[i][0]
         let endSeatsLeft = bestSeatsBigSaloon[i][1]
         let endSeatsRight = bestSeatsBigSaloon[i][2]
 
+        switch (this.selectedTypes) {
+        
+            case 4:
+            centerSeats = 1
+            break
+
+          case 5:
+          case 6:
+            centerSeats = 2
+            break
+          
+          case 7:
+          case 8:
+            centerSeats = 3
+            break
+          
+          case 9:
+          case 10:
+            centerSeats = 4
+            break
+          
+          case 11:
+          case 12:
+            centerSeats = 5
+            break
+
+        } 
         if (!this.currentShow.takenSeats[seats]) {
           
-          if (this.controlSeatsToRight(seats, endSeatsRight)) {
+          if (this.controlSeatsToRight((seats - centerSeats), endSeatsRight)) {
+            console.log('Letar åt höger', seats - centerSeats)
             return seatToRight
           }
-          else if (this.controlSeatsToleft(seats,endSeatsLeft)) {
+          else if (this.controlSeatsToleft((seats + centerSeats), endSeatsLeft)) {
+            console.log('letar åt vänster')
             return seatToLeft
           }
         }
       }
     }
+      alert(`No seats available together, please choose manualy`)
   }
+
+
 
   controlSeatsToRight(seats, end) {
     seatToRight = []
@@ -49,9 +76,9 @@ export default class SeatSelection {
   controlSeatsToleft(seats, end) {
     seatToLeft = []
     for (let j = 0; j < this.selectedTypes; j++){
-      console.log('this.currentShow.takenSeats[seats -j] < end', this.currentShow.takenSeats[seats -j] < end, 'end', end, 'seats -j', seats-j)
+      
       if (this.currentShow.takenSeats[seats -j]||(seats -j) < end ) {
-        console.log('Innan den bryter', this.currentShow.takenSeats[seats -j] < end)
+        
         seatToLeft = []
         break
       }
@@ -72,24 +99,32 @@ export default class SeatSelection {
       [42,37,46],
       [51,47,56],
       [52,47,56],
-      [61,57,68],
       [62,57,68],
-      [63,57,68],
+      [63, 57, 68],
+      [61,57,68],
       [64,57,68],
-      [73,69,80],
       [74,69,80],
-      [75,69,80],
+      [75, 69, 80],
+      [73,69,80],
       [76,69,80],
       [21,17,26],
       [22,17,26],
       [12,8,16],
       [13,8,16],
-      [85,81,92],
       [86,81,92],
       [87,81,92],
       [88, 81, 92],
+      [85,81,92],
       [3, 0, 7],
-      [4,0,7]
+      [4, 0, 7],
+      [30, 27, 36],
+      [33, 27, 36],
+      [40, 37, 46],
+      [43, 37, 46],
+      [50, 47, 56],
+      [53, 47, 56],
+      [60, 57, 68],
+      [65,57,68]
     ]
   }
     
